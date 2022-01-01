37f3d32b3c5c6ed64368bffed39bb9143e30b856
Luis Timaná <60704967+luistimana@users.noreply.github.com>
2021-12-31 20:41:15 -0500

    Obteniendo los personajes
    Obteniendo los personajes

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
   },
   mutations: {
+    setCharacters(state, playload) {
+      state.characters = playload
+    },
+    charactersFilter(state, playload) {
+      state.charactersFilter = playload
+    }
   },
   actions: {
+    async getCharacters({commit}) {
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        console.log(data)
+      } catch(error) {
+        console.error(error)
+      }
+    }
   },
   modules: {
   }
